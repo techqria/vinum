0eec39819e938b76a7a8db4132e2b6646e80aaed
alexandreoliv29 <alexandre.foj@gmail.com>
2022-11-15 18:12:35 -0300

    new-style

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -4,7 +4,7 @@ export const Alert: React.FC<any> = ({older, setOlder}) => {
 
     return (
         <section className="position-fixed bg-dark alert-card p-5 flex-column" style={{ display: older ? 'none' : 'flex' }}>
-            <h1 className="text-white">Você é maior de 18 anos?</h1>
+            <h2 className="text-white">Você é maior de 18 anos?</h2>
             <div className="d-flex flex-column">
                 <div className="d-flex gap-3 justify-content-center mt-3">
                     <button onClick={e => alert('Você não tem permissão para navegar neste site')} className="btn btn-danger">Não</button>
